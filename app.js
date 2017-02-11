ecd37a257a7617eb0f7f396e88f63b6c28129f65
fabilaszlo <fabilaszlo@gmail.com>
2017-02-11 15:41:28 +0100

    basics

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-var message = "hello world";
+var messageOriginal = "Top Secret? Really?";
+messageOriginal = messageOriginal.toUpperCase();
 
-console.log(message)
+var cryptABC_Basic = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var cryptABC=cryptABC_Basic;
+var distanceKey = 15;
 
+var cryptABCkeyword ="FÁBI LELLE LUCA";
+var cryptABCkeywordArray = Array.from(cryptABCkeyword);
+//console.log(cryptABCkeywordArray);
+
+var clear = function(array) {
+  for (var i=0; i<array.length; i++) {
+    if (array.indexOf(array[i]) <0) {
+      array[i]="";
+    }
+    else {
+      for (var j=i+1; j<array.length; j++) {
+        if (array[j] === array[i]) {
+          array[j] = "";
+        }
+      }  
+    }
+  }
+  return(array.join(''));
+}
+
+
+clear(cryptABCkeywordArray);
+console.log(clear);
