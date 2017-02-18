84d4b37cbfdc97dd8935c496f9171d19ed92dad1
fabilaszlo <fabilaszlo@gmail.com>
2017-02-18 23:27:49 +0100

    Caesar - re-formatted

--- caesar.js
+++ caesar.js
@@ -5,74 +5,56 @@ var messageCrypted = "";
 var cryptABC_Basic = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var cryptABC = [];
 var distanceKey = 3;
-var keyWord = "FÁBI LELLE";
+var keyWord = ("Fábi Horka Ákos").toUpperCase();
+keyWord = Array.from(keyWord);
 
-var clearString = function (string, array) {
-    return Array.from(string).filter(function (i) { return array.includes(i) }).join('');
-}
-/*  oldsschool version or rollCryptABC
-var rollCryptABC = function(array,number) {
-    var newarray = [];
-    for (var i=0; i<array.length; i++) {
-       newarray[i]= array[(i+distanceKey)%array.length];
+var clearText = function (text, characterBasis) {
+    if (typeof text !== 'string') {
+        return text.filter(function (i) { return characterBasis.includes(i) });
     }
-    return newarray;
-}
-// map without parameters
-var rollCryptABC = cryptABC_Basic.map(function(i){
-    return cryptABC_Basic[(distanceKey+cryptABC_Basic.indexOf(i))%cryptABC_Basic.length];
-})
-*/
-
-var removeDuplicationsFromString = function (string) {
-    return Array.from(string).reduce((acc, item) => acc.includes(item) ? acc : acc.concat(item), []).join('');
-}
-var removeDuplicationsFromArray = function (array) {
-    return array.reduce((acc, item) => acc.includes(item) ? acc : acc.concat(item));
+    else
+        return (Array.from(text)).filter(function (i) { return characterBasis.includes(i) }).join('');
 }
 
-var rollCryptABC = function (array, number) {
-    return array.map(function (i) { return array[(number + array.indexOf(i)) % array.length]; })
-}
-//
-/*  oldschool encryptCaesar function
-var enCryptCaesar = function(string, array1, array2) {
-    var newarray = [];
-    for (var i=0; i<string.length; i++) {
-        newarray[i] = array2[array1.indexOf(string[i])];
+var uniqText = function (text) {
+    if (typeof text === 'string') {
+        return (Array.from(text)).reduce((acc, item) => acc.includes(item) ? acc : acc.concat(item), []).join('');
     }
-    return newarray.join('');
-}
-*/
-var enCryptCaesar = function (string, array1, array2) {
-    return (Array.from(string).map(function (i) { return array2[array1.indexOf(i)]; })).join('');
+    else return text.reduce((acc, item) => acc.includes(item) ? acc : acc.concat(item), []);
 }
 
-var deCryptCaesar = function (string, array1, array2) {
-    return (Array.from(string).map(function (i) { return array1[array2.indexOf(i)]; })).join('');
+var rollCryptABC = function (basic, distance) {
+    return basic.map(function (i) { return basic[(distance + basic.indexOf(i)) % basic.length]; })
 }
 
-var cryptABC = rollCryptABC(cryptABC_Basic, distanceKey);
+var enCryptCaesar = function (text, basicABC, keyABC) {
+    return (Array.from(text).map(function (i) { return keyABC[basicABC.indexOf(i)]; })).join('');
+}
+
+var deCryptCaesar = function (text, basicABC, keyABC) {
+    return (Array.from(text).map(function (i) { return basicABC[keyABC.indexOf(i)]; })).join('');
+}
+
+var keyABC = function (key, basic) {
+    return uniqText(key.concat(rollCryptABC(basic, (basic.indexOf(key[key.length - 1]) + 1))));
+}
+
 console.log(cryptABC_Basic);
+cryptABC = rollCryptABC(cryptABC_Basic, distanceKey);
+console.log(distanceKey);
 console.log(cryptABC);
 
-console.log(messageOriginal);
-messageOriginal = clearString(messageOriginal, cryptABC_Basic);
 console.log(messageOriginal);
 messageCrypted = enCryptCaesar(messageOriginal, cryptABC_Basic, cryptABC);
 console.log(messageCrypted);
-
 console.log(deCryptCaesar(messageCrypted, cryptABC_Basic, cryptABC));
 
 console.log(keyWord);
-keyWord = Array.from(removeDuplicationsFromString(clearString(keyWord, cryptABC_Basic)));
+keyWord = uniqText(clearText(keyWord, cryptABC_Basic));
 console.log(keyWord);
 
-console.log(cryptABC_Basic.indexOf(keyWord[keyWord.length - 1]) + 1);
-cryptABC = Array.from(removeDuplicationsFromArray(keyWord.concat(rollCryptABC(cryptABC_Basic, (cryptABC_Basic.indexOf(keyWord[keyWord.length - 1]) + 1)))));
-
+cryptABC = keyABC (keyWord, cryptABC_Basic);
 console.log(cryptABC);
-console.log(messageOriginal);
 messageCrypted = enCryptCaesar(messageOriginal, cryptABC_Basic, cryptABC);
 console.log(messageCrypted);
 console.log(deCryptCaesar(messageCrypted, cryptABC_Basic, cryptABC));
